57038efb61977ad15d9bf6728b102635d6672c16
zenobit <6384793+zen0bit@users.noreply.github.com>
2021-11-24 11:17:45 +0100

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -12,3 +12,5 @@ cd 'void-mklive/'
 make clean
 make
 ./mklive.sh -a x86_64 -r 'https://mirrors.dotsrc.org/voidlinux/current' -p "$PKGS"
+./mklive.sh -a aarch64 -r 'https://mirrors.dotsrc.org/voidlinux/current/aarch64' -r 'https://mirrors.dotsrc.org/voidlinux/current/aarch64/nonfree' -p "$PKGS"
+./mklive.sh -a aarch64 -b rpi4-base -r 'https://mirrors.dotsrc.org/voidlinux/current/aarch64' -r 'https://mirrors.dotsrc.org/voidlinux/current/aarch64/nonfree' -p "$PKGS"
