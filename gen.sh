0dd5d59874db1738c4329fbab53cb544067dd658
zenobit <6384793+zen0bit@users.noreply.github.com>
2021-11-24 11:55:22 +0100

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 # Additional packages in the ISO
-PKGS="bzip2 cryptsetup curl dialog ddrescue gnupg2 git gptfdisk gzip efibootmgr lvm2 makepasswd mdadm mosh nano p7zip rsync wget vim unzip xz zip"
+PKGS="void-repo-nonfree rpi4-base rpi4-kernel rpi4-kernel-headers rpi-eeprom bzip2 cryptsetup curl dialog ddrescue gnupg2 git gptfdisk gzip efibootmgr lvm2 makepasswd mdadm mosh nano p7zip rsync wget vim unzip xz zip"
 
 yes y | xbps-install -Su xbps
 yes y | xbps-install -Su
-yes y | xbps-install -S git make
+yes y | xbps-install -S git make void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
 cd /root
 [ ! -d 'void-mklive' ] && git clone 'https://github.com/void-linux/void-mklive.git'
 cd 'void-mklive/'
 make clean
 make
-./mklive.sh -a x86_64 -r 'https://mirrors.dotsrc.org/voidlinux/current' -p "$PKGS"
-./mklive.sh -a aarch64 -r 'https://mirrors.dotsrc.org/voidlinux/current/aarch64' -r 'https://mirrors.dotsrc.org/voidlinux/current/aarch64/nonfree' -p "$PKGS"
-./mklive.sh -a aarch64 -b rpi4-base -r 'https://mirrors.dotsrc.org/voidlinux/current/aarch64' -r 'https://mirrors.dotsrc.org/voidlinux/current/aarch64/nonfree' -p "$PKGS"
+./mklive.sh -a aarch64 -r 'https://mirrors.dotsrc.org/voidlinux/current/aarch64' -p "$PKGS"
