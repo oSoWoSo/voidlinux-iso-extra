de0010166c292e69b06898e7c49b8d5beb7f038c
zenobit <6384793+zen0bit@users.noreply.github.com>
2021-11-27 00:14:48 +0100

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Additional packages in the ISO
-PKGS="rpi4-base bzip2 cryptsetup curl dialog ddrescue gnupg2 git gptfdisk gzip efibootmgr lvm2 makepasswd mdadm mosh nano p7zip rsync wget vim unzip xz zip"
+PKGS="rpi4-base bzip2 cryptsetup curl dialog ddrescue gnupg2 git gzip makepasswd mdadm mosh nano p7zip rsync wget vim unzip xz zip"
 
 yes y | xbps-install -Su xbps
 yes y | xbps-install -Su
-yes y | xbps-install -S git make cross-aarch64-linux-gnu cross-aarch64-linux-gnu-libc
+yes y | xbps-install -S git make coreutils cross-aarch64-linux-gnu cross-aarch64-linux-gnu-libc
 cd /root
 [ ! -d 'void-mklive' ] && git clone 'https://github.com/void-linux/void-mklive.git'
 cd 'void-mklive/'
