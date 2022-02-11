8f1e9b02b2bcab685c371227ed27ce007879f212
chtc <notnotcha0t1c@protonmail.com>
2022-02-11 14:59:27 +0100

    change stuff

--- gen.sh
+++ gen.sh
@@ -2,27 +2,47 @@
 set -euo pipefail
 
 # Additional packages in the ISO
-PKGS='bzip2 cryptsetup curl ddrescue gnupg2 git gptfdisk gzip efibootmgr lvm2 makepasswd mdadm mosh nano p7zip rsync wget vim unzip xz zip'
+PKGS='bzip2 cryptsetup curl gnupg git gptfdisk gzip efibootmgr lvm2 makepasswd mdadm p7zip wget unzip xz zip xorg-minimal xorg-fonts xf86-input-synaptics setxkbmap xbacklight xev xkill xmessage xprop xrandr xrdb base-devel mesa-dri vulkan-loader mesa-vulkan-radeon xf86-video-amdgpu mesa-vaapi mesa-vdpau bleachbit chromium chrony cmus cmus-flac cmus-opus cmus-pulseaudio cmusfm iwd dunst ffmpeg firefox fzf gimp herbstluftwm hsetroot htop imv keepassxc legendary maim mpv neovim newsboat nicotine+ opendoas opus opusfile opustags pamixer picard qbittorrent steam telegram-desktop trash-cli xclip xf86-input-evdev xonotic xonotic-data-low yt-dlp zathura zathura-pdf-mupdf pipewire unclutter libX11-devel libXft-devel libXinerama-devel qt5ct dmenu libXft mksh numix-solarized-gtk-theme slock st stylua ytfzf'
 
 # Base packages required for livecd
 # Source: https://github.com/void-linux/void-mklive/blob/master/build-x86-images.sh.in
 BASE_PKGS='dialog cryptsetup lvm2 mdadm void-docs-browse grub-i386-efi grub-x86_64-efi'
 
-# List of mirrors (see also: ci/set_repository.sh):
-#   https://docs.voidlinux.org/xbps/repositories/mirrors/
-REPO='https://mirrors.dotsrc.org/voidlinux/current'
+REPO='https://void.sakamoto.pl/current/'
+MULTILIB='https://void.sakamoto.pl/current/multilib'
+NONFREE='https://void.sakamoto.pl/current/nonfree'
 
-# Set repository
+# Set repository for xbps
 rm -fr /etc/xbps.d
 mkdir -p -m 755 /etc/xbps.d
-echo "repository=${REPO}" > /etc/xbps.d/repo.conf
+echo "repository=$REPO" > /etc/xbps.d/00-repository-main.conf
+echo "repository=$MULTILIB" > /etc/xbps.d/10-repository-multilib.conf
+echo "repository=$NONFREE" > /etc/xbps.d/10-repository-nonfree.conf
 
 xbps-install --yes -Su xbps
 xbps-install --yes -Su
 xbps-install --yes -S git make
 cd '/root'
+
+[ ! -d 'void-packages' ] && git clone --depth 1 'https://github.com/void-linux/void-packages.git' 'void-packages'
+[ ! -d 'custom-void-packages' ] && git clone --depth 1 'https://github.com/notchtc/custom-void-packages.git' 'custom-void-packages'
+
+cp -r custom-void-packages/* void-packages/srcpkgs
+
+cd void-packages
+
+./xbps-src binary-bootstrap
+
+custompkgs="connman dmenu libXft mksh numix-solarized-gtk-theme slock st stylua ytfzf"
+
+for i in $custompkgs; do
+    ./xbps-src pkg $i
+done
+
+cd '/root'
+
 [ ! -d 'void-mklive' ] && git clone -b master --single-branch --depth 1 'https://github.com/void-linux/void-mklive.git' 'void-mklive'
 cd 'void-mklive/'
 make clean
 make
-./mklive.sh -a 'x86_64' -r "${REPO}" -p "${BASE_PKGS} ${PKGS}"
+./mklive.sh -a 'x86_64' -r "/root/void-packages/hostdir/binpkgs" -r "${REPO}" -r "${MULTILIB}" -r "${NONFREE}" -p "${BASE_PKGS} ${PKGS}"
