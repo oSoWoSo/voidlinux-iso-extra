2c853855f5c199fdb719b30e7109844d610ca65e
chtc <notnotcha0t1c@protonmail.com>
2022-02-13 16:57:15 +0100

    maybe this is why it didnt work

--- gen.sh
+++ gen.sh
@@ -8,6 +8,7 @@ PKGS='bzip2 cryptsetup curl gnupg git gptfdisk gzip efibootmgr lvm2 makepasswd m
 # Source: https://github.com/void-linux/void-mklive/blob/master/build-x86-images.sh.in
 BASE_PKGS='dialog cryptsetup lvm2 mdadm void-docs-browse grub-i386-efi grub-x86_64-efi'
 
+CUSTOM='https://github.com/notchtc/custom-void-packages/raw/repo/current'
 REPO='https://void.sakamoto.pl/current'
 MULTILIB='https://void.sakamoto.pl/current/multilib'
 NONFREE='https://void.sakamoto.pl/current/nonfree'
@@ -28,4 +29,4 @@ cd '/root'
 cd 'void-mklive/'
 make clean
 make
-./mklive.sh -a 'x86_64' -r "${MULTILIB}" -r "${NONFREE}" -r "${REPO}" -p "${BASE_PKGS} ${PKGS}"
+./mklive.sh -a 'x86_64' -r "${MULTILIB}" -r "${NONFREE}" -r "${REPO}" "${CUSTOM}" -p "${BASE_PKGS} ${PKGS}"
